9ab599a1ec5cbe07e7a2555ffd9e174266ff66a7
Rianna Cleary <rianna.cleary@gmail.com>
2020-02-24 15:05:10 -0500

    more semantic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import {Header} from 'semantic-ui-react'
 
 class Home extends Component {
 
   render() {
     return (
-      <div>
-      Galaxybnb
+      <div className='home'>
+      <Header as='h1' inverted color ='violet' >Galaxybnb</Header>
       </div>
     );
   }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
+import { Menu, Segment } from 'semantic-ui-react'
 
 class NavBar extends Component {
 
